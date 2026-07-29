915392480fef25bf19c130cc4ba8ff93a9acd3c6
Fabiano Zero <fabiano.ortega@gmail.com>
2026-07-29 10:17:48 -0300

    release: v0.1.0.0

--- obj/Release/net9.0/JellyfinAutoPlayToggle.AssemblyInfo.cs
+++ obj/Release/net9.0/JellyfinAutoPlayToggle.AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("JellyfinAutoPlayToggle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.5.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.5.0+60fcea38d818189891fb219c776fbde1cc41bfe8")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0+a2403a4918235a7be17175e8561fed6088671fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("JellyfinAutoPlayToggle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JellyfinAutoPlayToggle")]
-[assembly: System.Reflection.AssemblyVersionAttribute("2.0.5.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
